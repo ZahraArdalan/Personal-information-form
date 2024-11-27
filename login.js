bf9216ce438c8484bc828230899605e731d5f2b9
zahra Ardalan <z.ardalan78@gmail.com>
2024-11-27 14:16:19 +0330

    edite

--- login.js
+++ login.js
@@ -7,38 +7,36 @@ const login = document.querySelector(".btn");
 
 login.addEventListener("click", validation);
 
-function validation () {
+function validation() {
+  // validate username input
+  if (username.value.length < 5) {
+    alert("Username must have at least 5 characters.");
+    return false;
+  }
 
+  // validate email input
+  let userEmail = email.value;
+  let pattern = new RegExp("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$");
+  let regexResult = pattern.test(userEmail);
+  console.log(regexResult);
+  if (!regexResult) {
+    alert("The email entered is not valid.");
+    return false;
+  }
 
-    
-      // validate username input
-      if (username.value.length < 5) {
-        alert("Username must have at least 5 characters.");
-        return false;
-      }
-    
-      // validate email input
-      let userEmail = email.value;
-      let pattern = new RegExp("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$");
-      let regexResult = pattern.test(userEmail);
-      if (!regexResult) {
-        alert("ایمیل وارد شده معتبر نمی باشد");
-        return false;
-      }
-    
-      // validate password input
-      if (password.value.length < 8) {
-        alert("پسورد حداقل باید 8 کاراکتر داشته باشد");
-        return false;
-      }
-    
-      if (password != passConfirm) {
-        alert("پسورد های وارد شده مطابقت ندارند");
-        return false;
-      }
-    
-      // success register
-      alert("ثبت نام شما با موفقیت انجام شد :)");
-      return true;
-    }
+  // validate password input
+  if (password.value.length < 8) {
+    alert("Password must be at least 8 characters long");
+    return false;
+  }
+
+  if (password != passConfirm) {
+    alert("The passwords entered do not match.");
+    return false;
+  }
+
+  // success register
+  alert("Your registration was successful :)");
+  return true;
+  validation.preventDefault();
 }
